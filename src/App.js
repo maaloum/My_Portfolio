26fd66cbef48800ad1985dba6146496081596948
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-01 23:32:14 +0300

    contact section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Leading from './components/Leading/Leading';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Project from './components/Project/Project';
+import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <About/>
         <Skills/>
         <Project/>
+        <Contact/>
     </div>
   );
 }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -11,7 +11,7 @@ export default function Project() {
                 <div class="card-body">
                     <h5 class="card-title text-primary">E-commerce</h5>
                     <p class="card-text">The application is built using the React library, which provides a fast and responsive user interface. The Bootstrap framework is used to create a modern and visually appealing design, while Skeleton provides a lightweight and responsive CSS boilerplate.</p>
-                    <a href="https://641f963ae0999f35015dbab1--glittering-elf-15ef96.netlify.app/"  target ="_blank" class="btn btn-info">Go Live</a>
+                    <a href="https://641f963ae0999f35015dbab1--glittering-elf-15ef96.netlify.app/"  target='_blank' rel="noreferrer" class="btn btn-info">Go Live</a>
                 </div>
             </div>
         <div class="card">
@@ -19,7 +19,7 @@ export default function Project() {
             <div class="card-body">
                 <h5 class="card-title text-primary">Maaloum Blog</h5>
                 <p class="card-text">Maaloum Blog is a React SPA application that allows users to view and interact with blog posts and to add new posts. It fetches all the information that it displays using a local API creates with a JSON server.</p>
-                <a href="https://main--super-paletas-b4ba8a.netlify.app/" target="_blank"  class="btn btn-info">Go Live</a>
+                <a href="https://main--super-paletas-b4ba8a.netlify.app/" target='_blank' rel="noreferrer" class="btn btn-info">Go Live</a>
             </div>
         </div>
         <div class="card">
@@ -27,7 +27,7 @@ export default function Project() {
             <div class="card-body">
                 <h5 class="card-title text-primary">Shopping Cart</h5>
                 <p class="card-text">This is a small application that I built using ReactJs. It is a little shop that displays products and lets users add or remove items to or from the cart page and calculate the total price of all itemes.</p>
-                <a href="#" target="_blank"  class="btn btn-info">Go Live</a>
+                <a href="/" target='_blank' rel="noreferrer"  class="btn btn-info">Go Live</a>
             </div>
         </div>
         <div class="card">
@@ -35,7 +35,7 @@ export default function Project() {
             <div class="card-body">
                 <h5 class="card-title text-primary">Country Info</h5>
                 <p class="card-text">Countries Infos is a React and Redux SPA application that displays interesting information about various countries in the world. It fetches all the information that it displays using API request to a server.</p>
-                <a href="#" target="_blank"  class="btn btn-info">Go Live</a>
+                <a href="/" target='_blank' rel="noreferrer" class="btn btn-info">Go Live</a>
             </div>
         </div>
         <div class="card">
@@ -43,7 +43,7 @@ export default function Project() {
             <div class="card-body">
                 <h5 class="card-title text-primary">Smart MS SA</h5>
                 <p class="card-text">Smart MS SA is a website built in Html5, Css3, Javascript/Bootstrap for a company that I worked for.</p>
-                <a href="https://www.smartmssa.com/" target="_blank"  class="btn btn-info">Go Live</a>
+                <a href="https://www.smartmssa.com/" target='_blank' rel="noreferrer"  class="btn btn-info">Go Live</a>
             </div>
         </div>
         <div class="card">
@@ -51,7 +51,7 @@ export default function Project() {
             <div class="card-body">
                 <h5 class="card-title text-primary">SmartDev</h5>
                 <p class="card-text">SmartDevis a website built in Html5, Css3, Javascript/Bootstrap for a company that I worked for.</p>
-                <a href="https://smartdev.ai/" target="_blank"  class="btn btn-info">Go live</a>
+                <a href="https://smartdev.ai/" target='_blank' rel="noreferrer"  class="btn btn-info">Go live</a>
             </div>
         </div>
         
