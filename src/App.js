0cc112706f91a2fa280c9715a02204b32cc916d8
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-01 18:34:01 +0300

    about page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import { Route, Routes } from 'react-router';
 import Leading from './components/Leading/Leading';
-
+import About from './components/About/About';
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <Leading/>
+        <About/>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css'
+
+export default function About() {
+  return (
+    <div className='About'>
+        <h3>About Me</h3>
+        <div className='contentAbout'>
+            <img src='./assets/s1.jpg' alt='coding_photo'/>
+            <p> I am a full Stack Developer with a passion for building beautiful and user-friendly websites.
+                I have a strong understanding of both back-end and front-end developement. I am excited to put 
+                my skills to use in a professional setting. I am a quick learner and a team player. I am confident that I can 
+                be a valuable asset to your company.  
+            </p>
+        </div>
+    </div>
+  )
+}
